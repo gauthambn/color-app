26f56f9846c871d3675e71928b97d6e128cf47ab
gauthambn <40561564+gauthambn@users.noreply.github.com>
2018-06-30 17:56:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,8 @@ color_codes = {
 }
 
 #color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue"])
-color = os.environ.get('APP_COLOR') or random.choice(["white"])
-#change done to test webhook
-#color = os.environ.get('APP_COLOR') or random.choice(["yellow","pink","purple"])
+#color = os.environ.get('APP_COLOR') or random.choice(["white"])
+color = os.environ.get('APP_COLOR') or random.choice(["yellow","pink","purple"])
 
 @app.route("/")
 def main():
