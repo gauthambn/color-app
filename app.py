6274432239fca08fe5511b39be7ef03877d7e417
gauthambn <40561564+gauthambn@users.noreply.github.com>
2020-01-23 14:54:31 +0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ color_codes = {
 }
 
 #color = os.environ.get('APP_COLOR') or random.choice(["red","green"])
-#color = os.environ.get('APP_COLOR') or random.choice(["white"])
+##color = os.environ.get('APP_COLOR') or random.choice(["white"])
 color = os.environ.get('APP_COLOR') or random.choice(["pink","blue"])
 
 @app.route("/")
